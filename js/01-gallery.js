44892d3ec95aa9607926b0f8ad1e886249457b80
Vadym4e <vadym.o92@gmail.com>
2023-02-19 13:12:56 +0200

    task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const makeGaleryCards = makeGalery(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", makeGaleryCards);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function makeGalery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(e) {
+  e.preventDefault();
+
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" alt="${e.target.alt} width = '1280px">
+    `
+  );
+  instance.show();
+
+  galleryContainer.addEventListener("keydown", (e) => {
+    if (e.code !== "Escape") {
+      return;
+    }
+    instance.close();
+  });
+}
