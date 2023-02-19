83653a99680dde893cbc7c1bc3f1470af4f48009
Vadym4e <vadym.o92@gmail.com>
2023-02-19 17:57:20 +0200

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const makeGaleryCards = makeGalery(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", makeGaleryCards);
+
+// galleryContainer.addEventListener("click", onGalleryContainerClick);
+// galleryContainer.preventDefault();
+function makeGalery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+`;
+    })
+    .join("");
+}
+
+// function onGalleryContainerClick(e) {
+//   e.preventDefault();
+
+//   //   if (!e.target.classList.contains("gallery__image")) {
+//   //     return;
+//   //   }
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
+// }
